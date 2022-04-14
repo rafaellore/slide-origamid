f34b6fbfc37700514710ed69c5341bd7551985cd
Rafael Moreira <rafael.moreira27099@gmail.com>
2022-04-13 23:32:22 -0300

    final da navegando prev e next

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ import Slide from "./slide.js";
 const slide = new Slide(".slide", ".slide-wrapper");
 slide.init();
 console.log(slide);
-slide.changeSlide(2);
+
+slide.changeSlide(3);
+slide.activePrevSlide();
